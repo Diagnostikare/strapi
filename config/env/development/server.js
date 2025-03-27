3d45213a4c28317165081c084617f583a84c22b0
Axel Tinoco <axeltm8@gmail.com>
2025-03-27 17:24:50 -0600

    Sandbox URL

--- config/env/development/server.js
+++ config/env/development/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
-    url: env('PUBLIC_URL', 'https://strapi.diagnostikare.com'),
+    url: env('PUBLIC_URL', 'https://strapi-sandbox.diagnostikare.com'),
     proxy: true
   });
