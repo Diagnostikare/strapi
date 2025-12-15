43d5192f6efa010a9e8cef526113b40d90121fe3
Majo Cervantes <majocervantes@MacBook-Pro-de-Majo.local>
2025-12-15 16:00:57 -0600

    Add ServiceFlow colection

---
+++ src/api/service-flow/controllers/service-flow.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * service-flow controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::service-flow.service-flow');
---
+++ src/api/service-flow/services/service-flow.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * service-flow service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::service-flow.service-flow');
