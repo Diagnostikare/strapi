c3f08e79b68cacccbedbd03ee12df5ffeb0f7aa8
lamchang <alanlamchang@gmail.com>
2026-02-06 21:14:34 -0600

    Wizard PR

--- src/api/site/controllers/site.js
+++ src/api/site/controllers/site.js
@@ -6,4 +6,35 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::site.site');
+module.exports = createCoreController('api::site.site', ({ strapi }) => ({
+  async find(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        themes: {
+          populate: {
+            logo: true,
+            header: true,
+            primary_button: true,
+            secondary_bottom: true,
+            tertiary_button: true,
+            cta_button: true,
+            dangerous_button: true,
+            outline_button: true,
+            disabled_button: true,
+            splash: {
+              populate: '*', // incluye cta (pwa.button) dentro de splash
+            },
+            input_default: true,
+            input_active: true,
+            input_error: true,
+            input_disabled: true,
+            wizard: true,
+          },
+        },
+      },
+    };
+
+    return await super.find(ctx);
+  },
+}));
