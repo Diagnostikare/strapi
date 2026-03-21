b99f5712b9670baa29ec382b92d1e45be59b6554
Majo Cervantes <champiis49@gmail.com>
2026-03-20 18:25:51 -0600

    Feat: Hidden feature flag

---
+++ src/api/hidden-feature/controllers/hidden-feature.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hidden-feature controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::hidden-feature.hidden-feature');
