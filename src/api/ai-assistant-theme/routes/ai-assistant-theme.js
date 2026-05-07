fe9e2ac1587b685f279e958e135d10273e70700d
Axel Tinoco <axeltm8@gmail.com>
2026-05-07 13:53:33 -0600

    feat(*) AI Theme Component

---
+++ src/api/ai-assistant-theme/routes/ai-assistant-theme.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ai-assistant-theme router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ai-assistant-theme.ai-assistant-theme');
